1b22a6eccc6a0b4a021d12266133eaad4435225a
Gaurav Jawalkar <gauravjawalkar8@gmail.com>
2025-10-26 18:16:48 +0530

    migrated the Postgres to hosted database to skip the docker containerization

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,16 @@
 import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+
+const neonPostgresPool = new Pool(
+    {
+        connectionString: process.env.DATABASE_URL!,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+)
 
 // postgres://<username>:<password>@<host>:<port>/<db_name> ---> Postgres database URL convention
-const db = drizzle(process.env.DATABASE_URL! || "postgres://gaurav:admin@localhost:5432/learndb");
+const db = drizzle(neonPostgresPool);
 
 export { db }
